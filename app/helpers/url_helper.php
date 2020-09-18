bba19fdf8360178f689a4f0200bc7ed952c5790b
Limage Aurore <limageaurore@gmail.com>
2020-09-18 16:25:00 +0200

    Update url_helper.php

--- app/helpers/url_helper.php
+++ app/helpers/url_helper.php
@@ -1,14 +1,12 @@
 <?php
-
-<<<<<<< HEAD
     // Simple page redirect
     function redirect($page)
     {
         header('location: ' . URLROOT . '/' . $page);
     }
-=======
+
 //Simple page redirect
 function redirect($page){
     header("Location: " . URLROOT . "/" . $page);
 }
->>>>>>> origin/mohamed
+
